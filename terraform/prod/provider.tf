dcd96e77aed903920f0b543c9613c7dbfea9f8c1
Amazon Q <amazonq@amazon.com>
2025-11-03 21:34:17 +0200

    Fix provider.tf formatting in prod environment

--- terraform/prod/provider.tf
+++ terraform/prod/provider.tf
@@ -1,19 +1,14 @@
 terraform {
   required_providers {
-
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-
   }
 }
 
-
-
-
 provider "aws" {
-  region  = "us-east-2"
+  region = "us-east-2"
 
   default_tags {
     tags = {
