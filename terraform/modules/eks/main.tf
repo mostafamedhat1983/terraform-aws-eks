c3a61463d44ec8785f378b2aaf8681340978d5ca
= <=>
2025-11-12 03:15:08 +0200

    Update KMS alias name to include env name for better identification

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -20,7 +20,7 @@ resource "aws_kms_key" "this" {
 }
 
 resource "aws_kms_alias" "this" {
-  name          = "alias/eks-cluster-key"
+  name          = "alias/eks-cluster-key-${var.cluster_name}"
   target_key_id = aws_kms_key.this.key_id
 }
 
